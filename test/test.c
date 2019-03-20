abfd046be2cac48ae02da63a298a59d779898dff
tnicolas42 <tnicolas@student.42.fr>
2019-03-20 18:20:36 +0100

    increase tests

--- test/test.c
+++ test/test.c
@@ -6,7 +6,7 @@
 /*   By: tnicolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/20 18:03:57 by tnicolas          #+#    #+#             */
-/*   Updated: 2019/03/20 18:06:40 by tnicolas         ###   ########.fr       */
+/*   Updated: 2019/03/20 18:19:57 by tnicolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 
 int			main(int ac, char **av)
 {
-	void *test;
+	int		size;
+	char	*test;
 
 	(void)ac;
 	(void)av;
-	test = malloc(4);
+	size = 20;
+	ft_printf("malloc of size %d with * at pos 8\n", size);
+	test = malloc(size);
+	test[8] = 42;
+	print_alloc_mem(test, size);
+	show_alloc_mem();
+	ft_putstr("free all\n");
+	free_all();
+	show_alloc_mem();
 	return (0);
 }
